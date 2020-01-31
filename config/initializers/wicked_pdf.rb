59f2d562f4e9cb9f6fe4215a4bfe73823debfad2
Masyrafpauzi <acap_sum41@hotmail.com>
2020-01-31 17:20:22 +0800

    fix wicked pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -13,7 +13,20 @@ WickedPdf.config = {
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   #   or
-  exe_path:  "C:/Ruby26-x64/bin/wkhtmltopdf.exe"
+  if Rails.env.production?
+    wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+  else
+    # Linux (check your processor for Intel x86 or AMD x64)
+    # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+    # wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-i386"
+    # OS X
+    exe_path:  "C:/Ruby26-x64/bin/wkhtmltopdf.exe"           
+    # Windows
+    # wkhtmltopdf_path = 'C:\Program Files/wkhtmltopdf/wkhtmltopdf.exe'
+  end
+  
+  WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
+
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
