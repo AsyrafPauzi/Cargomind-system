4a00601121210fc945b381ea84551e269749cff0
Masyrafpauzi <acap_sum41@hotmail.com>
2020-03-08 01:14:24 +0800

    Update Fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,10 @@ CarrierWave.configure do |config|
         config.fog_credentials = {
           :provider => 'aws',
           :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-          :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY_ID'],
+          :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
           :region => 'ap-southeast-1'
         }
-        config.fog_directory = ENV['S3_BUCKET_NAME']
+        config.fog_directory = ENV['S3_BUCKET']
         config.storage = :fog
       else
         config.fog_provider = 'fog/aws' 
